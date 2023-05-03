24b2328648bcfb8f1c31980f513fd6c678c36db6
João Pedro das Chagas Ferrara <joaopedroferrara@gmail.com>
2023-05-03 15:24:05 -0300

    commit inicial

---
+++ core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Marca, Veiculo, Pessoa, Parametros, MovRotativo
+
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin','checkout','valor_hora','pago', 'total', 'horas_total')
+    
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
+admin.site.register(Parametros)
+admin.site.register(MovRotativo, MovRotativoAdmin)
